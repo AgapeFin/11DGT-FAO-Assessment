874383f0b6bcf276ab5b1a33e4a16ad7e1c04722
AgapeFin <agape.fineanganofo@marcellin.school.nz>
2025-10-31 19:33:27 +1300

    Added an inputs for the user.
    Added input in which the system asks for the users name, then asks the user how many questions it would like.
    
    An addition to this is that I have added some workarounds just in case the user tries to input 0 questions, or decimals.

--- Basic-Math-Quiz.py
+++ Basic-Math-Quiz.py
@@ -1,1 +1,15 @@
+
+
 print("Welcome to the Basic Maths Quiz for Dummies!")
+
+
+name = input("What is your name? ").strip()
+while True:
+    try:
+        total_questions = int(input("How many questions would you like? "))
+        if total_questions <= 0:
+            print("Please enter a number that is greater then 0. ")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid number (no decimals). ")
