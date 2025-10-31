7b32ffbdb5e8462caa4abe1f9d6ab261107c661f
AgapeFin <agape.fineanganofo@marcellin.school.nz>
2025-10-31 20:21:14 +1300

    Fixed some code, added new input

--- Basic-Math-Quiz.py
+++ Basic-Math-Quiz.py
@@ -1,6 +1,5 @@
 
-
-print("Welcome to the Basic Maths Quiz for Dummies!")
+print("Welcome to the Basic Facts Quiz!")
 
 
 name = input("What is your name? ").strip()
@@ -8,8 +7,19 @@ while True:
     try:
         total_questions = int(input("How many questions would you like? "))
         if total_questions <= 0:
-            print("Please enter a number that is greater then 0. ")
+            print("Please enter a number greater than 0.")
             continue
         break
     except ValueError:
-        print("Please enter a valid number (no decimals). ")
+        print("Please enter a valid whole number.")
+operations = ["+", "-", "×", "÷"]
+
+
+print("\nChoose operation type:")
+print("1. Addition (+)\n2. Subtraction (-)\n3. Multiplication (×)\n4. Division (÷)\n5. Mixed")
+while True:
+    choice = input("Enter 1–5: ").strip()
+    if choice in ["1", "2", "3", "4", "5"]:
+        break
+    else:
+        print("Invalid choice. Try again.")
